d832ada10a0a800efd1b5f9d4c2c0fb1d596a5af
behappymashka <98Mashenka@gmail.com>
2024-02-11 16:13:54 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Вивести на сторінку в один рядок через кому числа від 10 до 20
+
+let number = 10;
+
+do {
+    document.write(`${number}, `);
+    number++;
+} while (number < 20);
+
+document.write(`${number}.`);
+
+
---
+++ js/main2.js
@@ -0,0 +1,5 @@
+// Вивести квадрати чисел від 10 до 20
+let number;
+for (number = 10; number <= 20; number++) {
+    document.write(`${number ** 2} `);
+}
